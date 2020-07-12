472f8ccb90dc1e068eca347627752584fc039ac1
durbcodes <nick.durbin@gmail.com>
2020-07-12 03:42:33 -0400

    MVP complete.

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,15 +1,24 @@
 def linear_search(arr, target):
     # Your code here
-    # Tetsing
-
+    for item in range(0, len(arr)):
+        if arr[item] == target:
+            return item
 
     return -1   # not found
 
-
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
     # Your code here
+    start = 0
+    end = len(arr) - 1
 
+    while start <= end:
+        middle = (start + end) // 2
+        if arr[middle] == target:
+            return middle
+        elif arr[middle] < target:
+            start = middle + 1
+        else:
+            end = middle - 1
 
     return -1  # not found
